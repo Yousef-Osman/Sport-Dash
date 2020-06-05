96b1ee479104cbb6d6c0b2b8ed137de5a0d93419
Abanob-Hanna <54998812+Abanob-Hanna@users.noreply.github.com>
2020-06-06 00:47:10 +0200

    added search view model

--- SportDash/ViewModels/DataViewModel.cs
+++ SportDash/ViewModels/DataViewModel.cs
@@ -20,20 +20,7 @@ namespace SportDash.ViewModels
         public IEnumerable<Review> Reviews { get; set; } // +R
         public Review Review { get; set; } // +R
 
-        // Search View Model
-        public List<ApplicationUser> Gyms { get; set; }  
-        public List<ApplicationUser> Playgrounds { get; set; }
-        public List<ApplicationUser> Trainers { get; set; }
-        public List<SelectListItem> Category { get; set; }
-        public List<SelectListItem> Price { get; set; }
-        public List<SelectListItem> Location { get; set; }
-        public List<SelectListItem> SportType { get; set; }
-        public string SearchString { get; set; }
-        public string CategoryString { get; set; }
-        public string PriceString { get; set; }
-        public string LocationString { get; set; }
-        public string SportTypeString { get; set; }
-
+ 
         public IEnumerable<PlaygroundReservation> Reservations { get; set; }
         public IEnumerable<PlaygroundReservation> Requests { get; set; }
         public IEnumerable<PlaygroundReservation> AllReservations { get; set; }
