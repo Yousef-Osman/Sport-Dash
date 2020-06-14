ed9d1ce19d65a44a324d1a46c6c25141b6809ca9
Yousef Osman <yousef.osman.31@gmail.com>
2020-06-14 09:47:46 +0200

    Updating home page

--- SportDash/Controllers/HomeController.cs
+++ SportDash/Controllers/HomeController.cs
@@ -33,11 +33,6 @@ namespace SportDash.Controllers
             //return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
