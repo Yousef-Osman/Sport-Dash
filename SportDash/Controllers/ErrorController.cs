572f8e9f4bd69243dd8166e25f1b70e2ce6df632
ahmedmagdy <ahmedmagdy492@yahoo.com>
2020-06-15 13:14:27 +0200

    added error page

---
+++ SportDash/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportDash.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(int errorCode)
+        {
+            ViewBag.code = errorCode;
+            return View();
+        }
+    }
+}
