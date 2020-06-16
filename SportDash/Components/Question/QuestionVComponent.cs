0e8a855424bee1b3fc0593c544479bd76c253259
Yousef Osman <yousef.osman.31@gmail.com>
2020-06-16 12:57:01 +0200

    Modified Questions UI

--- SportDash/Components/Question/QuestionVComponent.cs
+++ SportDash/Components/Question/QuestionVComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportDash.Models;
+using SportDash.Repository;
 using SportDash.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,30 @@ namespace SportDash.Components.Question
 {
     public class QuestionVComponent : ViewComponent
     {
+        private readonly IImageRepository _imageRepository;
+
+        public QuestionVComponent(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
         public IViewComponentResult Invoke(DataViewModel dataViewModel)
         {
+            dataViewModel.ImagePath = GetImagePath(dataViewModel.Question.UserId);
             return View("/Components/Question/QuestionViewComponent.cshtml", dataViewModel);
         }
 
+        public string GetImagePath(string id)
+        {
+            Image image = _imageRepository.GetImages(id).Where(img => img.IsProfileImg).FirstOrDefault();
+            if (image == null)
+            {
+                return "/images/site/user-icon.jpg";
+            }
+            else
+            {
+                return "/images/" + image.Title;
+            }
+        }
     }
+
 }
