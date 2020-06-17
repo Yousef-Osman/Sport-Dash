a5e436e352e66eb1bee39d4e69da9c33d5b7f225
Mina medhat <minamedhat_fcis@hotmail.com>
2020-06-17 12:40:35 +0200

    final sync

--- SportDash/Controllers/GymController.cs
+++ SportDash/Controllers/GymController.cs
@@ -168,19 +168,30 @@ namespace SportDash.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddReview(Review R)
+        public async Task<IActionResult> AddReview(Review R)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
+            DataViewModel reviewVM = new DataViewModel();
+            var userId = R.ReviewerId;
+            var TargetId = R.TargetId;
             var review = _reviewRepository.PostReview(R);
-            return Ok(new OkObjectResult(review));
+            reviewVM.ProfileImage = _imageRepository.GetImages(userId).Where(a => a.IsProfileImg == true).SingleOrDefault();
+            reviewVM.CurrentUser = await _userManager.GetUserAsync(User);
+            reviewVM.Reviews = _reviewRepository.GetReviewsOfReviewee(TargetId);
+            reviewVM.Review = review;
+            return PartialView("_ShowReviews", reviewVM);
         }
 
         [HttpPost]
-        public ActionResult EditPricePerPeriod(GymPrices gymPrice)
+        public async Task<IActionResult> DeleteReview(int id, string TargetId)
         {
-            var IsEntered = _gymPriceRepository.AddOrEditPricePerPeriod(gymPrice);
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            DataViewModel reviewVM = new DataViewModel();
+            reviewVM.CurrentUser = await _userManager.GetUserAsync(User);
+            _reviewRepository.DeleteReview(id);
+            reviewVM.Reviews = _reviewRepository.GetReviewsOfReviewee(TargetId);
 
-            return Ok(new JsonResult(IsEntered));
+            return PartialView("_ShowReviews", reviewVM);
         }
     }
 }
